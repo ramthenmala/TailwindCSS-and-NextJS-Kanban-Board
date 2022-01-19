18828a889500fba0e20445826077c4d419e2f500
Ram kumar <webbieram@outlook.com>
2022-01-19 13:23:29 +0530

    updated with draggable option

--- components/Board.js
+++ components/Board.js
@@ -1,25 +1,70 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { useState, useEffect } from "react";
 import { DotsVerticalIcon } from "@heroicons/react/outline";
 import CardItem from "./Card";
 import BoardData from "../data/boarddata.json";
 
 function Board() {
+  const [ready, setReady] = useState(false);
+  const [boardData, setBoardData] = useState(BoardData);
+
+  useEffect(() => {
+    if (process.browser) {
+      setReady(true);
+    }
+  });
+
+  const onDragEnd = (re) => {
+    if (!re.destination) return;
+    let newBoardData = boardData;
+    var dragItem =
+      newBoardData[parseInt(re.source.droppableId)].items[re.source.index];
+    newBoardData[parseInt(re.source.droppableId)].items.splice(
+      re.source.index,
+      1
+    );
+    newBoardData[parseInt(re.destination.droppableId)].items.splice(
+      re.destination.index,
+      0,
+      dragItem
+    );
+    setBoardData(newBoardData);
+  };
+
   return (
     <>
-      <div className="grid grid-cols-4 gap-2">
-        {BoardData.map((board, index) => (
-          <div className="bg-white shadow-md rounded-md " key={index}>
-            <h4 className="flex justify-between items-center p-2">
-              <span className="uppercase text-xs font-semibold text-gray-600 tracking-[.25em]">
-                {board.name}
-              </span>
-              <DotsVerticalIcon className="w-4 h-4 text-gray-500 mb-4" />
-            </h4>
-            {board.items.map((item, iIndex) => (
-              <CardItem key={iIndex} data={item} />
+      {ready && (
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="grid grid-cols-4 gap-2">
+            {BoardData.map((board, index) => (
+              <div key={index} className="bg-white shadow-md rounded-md ">
+                <Droppable droppableId={index.toString()}>
+                  {(provider) => (
+                    <div {...provider.droppableProps} ref={provider.innerRef}>
+                      <h4 className="flex justify-between items-center p-2">
+                        <span className="uppercase text-xs font-semibold text-gray-600 tracking-[.25em]">
+                          {board.name}
+                        </span>
+                        <DotsVerticalIcon className="w-4 h-4 text-gray-500 mb-4" />
+                      </h4>
+                      {board.items.length > 0 &&
+                        board.items.map((item, iIndex) => {
+                          return (
+                            <CardItem
+                              key={item.id}
+                              data={item}
+                              index={iIndex}
+                            />
+                          );
+                        })}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
             ))}
           </div>
-        ))}
-      </div>
+        </DragDropContext>
+      )}
     </>
   );
 }
--- components/Card.js
+++ components/Card.js
@@ -3,71 +3,81 @@ import {
   PaperClipIcon,
   PlusCircleIcon,
 } from "@heroicons/react/outline";
+import { Draggable } from "react-beautiful-dnd";
 
 import Image from "next/image";
 
-function CardItem({ data }) {
+function CardItem({ data, index }) {
   return (
-    <div className="bg-gray-100 rounded-sm shadow-sm p-2 m-2">
-      <label
-        className={`uppercase text-xs px-3 py-1 text-white bg-green-400 rounded-full ${
-          data.priority === 0
-            ? "bg-green-400"
-            : data.priority === 1
-            ? "bg-red-400"
-            : "bg-red-900"
-        }`}
-      >
-        {data.priority === 0
-          ? "Low Priority"
-          : data.priority === 1
-          ? "Medium Priority"
-          : "High Priority"}
-      </label>
-      <h2 className="text-gray-800 pt-2 text-sm pb-2 mb-2 font-semibold">
-        {data.title}
-      </h2>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center justify-between -space-x-3">
-          {data.assignees &&
-            data.assignees.map((imgurl, iIMGIndex) => (
-              <div
-                key={iIMGIndex}
-                className="overflow-hidden border-white border-2 z-10 rounded-full w-8 h-8 cursor-pointer"
-              >
-                <Image
-                  src={imgurl.avt}
-                  alt="img"
-                  objectFit="cover"
-                  className="rounded-full overflow-hidden"
-                  width="30"
-                  height="30"
-                />
-              </div>
-            ))}
+    <Draggable index={index} draggableId={data.id.toString()}>
+      {(provided) => (
+        <div
+          className="bg-gray-100 rounded-sm shadow-sm p-2 m-2"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <label
+            className={`capitalize py-1 px-2 text-xs text-white  rounded-full ${
+              data.priority === 0
+                ? "bg-green-600"
+                : data.priority === 1
+                ? "bg-yellow-400"
+                : "bg-red-600"
+            }`}
+          >
+            {data.priority === 0
+              ? "Low priority"
+              : data.priority === 1
+              ? "Medium Priority"
+              : "High Priority"}
+          </label>
+          <h2 className="text-gray-800 pt-2 text-sm pb-2 mb-2 font-semibold">
+            {data.title}
+          </h2>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between -space-x-3">
+              {data.assignees &&
+                data.assignees.map((imgurl, iIMGIndex) => (
+                  <div
+                    key={iIMGIndex}
+                    className="overflow-hidden border-white border-2 z-10 rounded-full w-8 h-8 cursor-pointer"
+                  >
+                    <Image
+                      src={imgurl.avt}
+                      alt="img"
+                      objectFit="cover"
+                      className="rounded-full overflow-hidden"
+                      width="30"
+                      height="30"
+                    />
+                  </div>
+                ))}
+            </div>
+
+            <div className="flex space-x-3 text-gray-600 items-center justify-between">
+              {data.chat > 0 && (
+                <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
+                  <ChatAlt2Icon className="w-4 h-4 " />
+                  <span>{data.chat}</span>
+                </span>
+              )}
+
+              {data.attachment > 0 && (
+                <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
+                  <PaperClipIcon className="w-4 h-4 " />
+                  <span>{data.attachment}</span>
+                </span>
+              )}
+
+              <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
+                <PlusCircleIcon className="w-6 h-6 text-gray-400" />
+              </span>
+            </div>
+          </div>
         </div>
-
-        <div className="flex space-x-3 text-gray-600 items-center justify-between">
-          {data.chat > 0 && (
-            <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
-              <ChatAlt2Icon className="w-4 h-4 " />
-              <span>{data.chat}</span>
-            </span>
-          )}
-
-          {data.attachment > 0 && (
-            <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
-              <PaperClipIcon className="w-4 h-4 " />
-              <span>{data.attachment}</span>
-            </span>
-          )}
-
-          <span className="flex items-center justify-between space-x-1 text-sm cursor-pointer">
-            <PlusCircleIcon className="w-6 h-6 text-gray-400" />
-          </span>
-        </div>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 }
 
